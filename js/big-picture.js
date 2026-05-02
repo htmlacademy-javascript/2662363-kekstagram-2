be1a6aed20da7cfe802dca3ffb52f3ffbc2967ff
Alex <aleksandr_abrame@mail.ru>
2026-05-02 16:52:28 +0300

    Show the comment counter block

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,12 +1,23 @@
+/* eslint-disable no-unused-vars */
+const COMMENTS_STEP = 5;
 const body = document.body;
 const modalNode = document.querySelector('.big-picture');
 const closeButtonNode = modalNode.querySelector('.big-picture__cancel');
 const imageNode = modalNode.querySelector('.big-picture__img img');
 const captionNode = modalNode.querySelector('.social__caption');
 const likesNode = modalNode.querySelector('.likes-count');
-const totalCommentsNode = modalNode.querySelector('.social__comment-total-count');
+const totalCommentsNode = modalNode.querySelector(
+  '.social__comment-total-count',
+);
 const commentsListNode = modalNode.querySelector('.social__comments');
 const commentTemplate = modalNode.querySelector('.social__comment');
+const commetLoaderNode = modalNode.querySelector('.comments-loader');
+const commentsShownNode = modalNode.querySelector(
+  '.social__comment-shown-count',
+);
+
+let localComments;
+let renderedComments;
 
 const showModal = (isVisible = true) => {
   if (isVisible) {
@@ -26,34 +37,62 @@ const showModal = (isVisible = true) => {
 const onDocumentKeydown = (evt) => {
   if (evt.key === 'Escape') {
     showModal(false);
-
   }
+};
+
+const renderStatistic = () => {
+  commentsShownNode.textContent = renderedComments;
+};
+
+const renderLoader = () => {
+  if (localComments.length) {
+    commetLoaderNode.classList.remove('hidden');
+    return;
+  }
+  commetLoaderNode.classList.add('hidden');
+};
+
+const renderComments = () => {
+  const fragment = document.createDocumentFragment();
+
+  localComments
+    .splice(0, COMMENTS_STEP)
+    .forEach(({ avatar, message, name }) => {
+      const newCommentNode = commentTemplate.cloneNode(true);
+      const avatarNode = newCommentNode.querySelector('.social__picture');
+      avatarNode.src = avatar;
+      avatarNode.alt = name;
+      newCommentNode.querySelector('.social__text').textContent = message;
+      fragment.append(newCommentNode);
+
+      renderedComments++;
+    });
+  commentsListNode.append(fragment);
+  renderStatistic();
+  renderLoader();
+};
+
+const render = ({ url, description, likes }) => {
+  imageNode.src = url;
+  captionNode.textContent = description;
+  likesNode.textContent = likes;
+  totalCommentsNode.textContent = localComments.length;
+
+  commentsListNode.innerHTML = '';
+  renderComments();
 };
 
 export const openModal = ({ url, description, comments, likes }) => {
   showModal();
-
-  imageNode.src = url;
-  captionNode.textContent = description;
-  likesNode.textContent = likes;
-  totalCommentsNode.textContent = comments.length;
-
-  commentsListNode.innerHTML = '';
-
-  const fragment = document.createDocumentFragment();
-
-  comments.forEach(({ avatar, message, name }) => {
-    const newCommentNode = commentTemplate.cloneNode(true);
-    const avatarNode = newCommentNode.querySelector('.social__picture');
-    avatarNode.src = avatar;
-    avatarNode.alt = name;
-    newCommentNode.querySelector('.social__text').textContent = message;
-    fragment.append(newCommentNode);
-  });
-
-  commentsListNode.append(fragment);
+  localComments = [...comments];
+  renderedComments = 0;
+  render({ url, description, comments, likes });
 };
 
 closeButtonNode.addEventListener('click', () => {
   showModal(false);
+});
+
+commetLoaderNode.addEventListener('click', () => {
+  renderComments();
 });
--- js/render-cards.js
+++ js/render-cards.js
@@ -1,25 +1,36 @@
+/* eslint-disable no-unused-vars */
 import { openModal } from './big-picture.js';
 
 /* eslint-disable no-console */
 const cardTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const cardsContainer = document.querySelector('.pictures');
 
+let localPhotos;
+
 export const renderCards = (pictures) => {
-  console.log(pictures);
+  localPhotos = [...pictures];
   const fragment = document.createDocumentFragment();
-  pictures.forEach(({url, description, comments, likes}) => {
+  pictures.forEach(({ url, description, comments, likes, id }) => {
     const card = cardTemplate.cloneNode(true);
     const image = card.querySelector('.picture__img');
     image.src = url;
     image.alt = description;
     card.querySelector('.picture__comments').textContent = comments.length;
     card.querySelector('.picture__likes').textContent = likes;
-
-    card.addEventListener('click', () => {
-      openModal({ url, description, comments, likes });
-    });
-
+    card.dataset.id = id;
     fragment.append(card);
   });
   cardsContainer.append(fragment);
 };
+
+cardsContainer.addEventListener('click', (evt) => {
+  const card = evt.target.closest('.picture');
+  if (card) {
+    const id = Number(card.dataset.id);
+    const photo = localPhotos.find((item) => item.id === id);
+    if (photo) {
+      openModal(photo);
+    }
+  }
+});
+
